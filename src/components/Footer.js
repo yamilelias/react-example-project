36dbe1242754990e5748cb84fb51837166f5ad03
yamilelias <yamileliassoto@gmail.com>
2023-04-17 18:58:57 -0600

    create components

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import { Card } from './Card'
+
+export const Footer = ({ inter }) => (
+    <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Card
+            inter={inter}
+            title="Deploy"
+            url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            body="Instantly deploy your Next.js site to a shareable URL with Vercel." />
+        <Card
+            inter={inter}
+            title="Deploy"
+            url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            body="Instantly deploy your Next.js site to a shareable URL with Vercel." />
+        <Card
+            inter={inter}
+            title="Deploy"
+            url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            body="Instantly deploy your Next.js site to a shareable URL with Vercel." />
+        <Card
+            inter={inter}
+            title="Deploy"
+            url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            body="Instantly deploy your Next.js site to a shareable URL with Vercel." />
+      </div>
+)
